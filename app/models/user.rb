035d30271618d09760296673ed13722cfbca429e
joeysk2012 <kurokaw1@gmail.com>
2017-12-14 17:29:19 -0800

    twitter auth finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
 
   
   def self.from_omniauth(auth)
-    p auth
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.nickname + "@twitter.com"
+      user.email = auth.info.nickname
       user.oauth_token = auth.credentials.token
       user.password = SecureRandom.urlsafe_base64
       user.save!
