1dc4acb8a29b27e2c41229367b3d3a630ae25152
joeysk2012 <kurokaw1@gmail.com>
2018-03-05 07:39:32 -0800

    second try at vendor assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 #Rails.application.config.assets.precompile += %w(admin.js admin.css graph.js c3.min.js)
 Rails.application.config.assets.precompile = ['*.js', '*.css']
+Rails.application.config.assets.precompile += %w( landing-page.css ) 
